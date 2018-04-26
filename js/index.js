98007b000aea33c14945bd3f3fc14061304ee75c
Todd Eidson <todd@eidson.info>
2018-04-25 21:03:43 -0400

    Live read file & watch for pressure drop

--- js/index.js
+++ js/index.js
@@ -1,16 +1,45 @@
-fs = require('fs');
-      fs.readFile('4.21.18 test.txt', 'ascii', function(err, data) {
-        lines = data.split(/\r?\n/);
-        mapped = lines.reduce(function(result, line){
-          if(!line.includes("System")) {
-            obj = line.split( /\s+/);
-            obj[1] = Number(obj[1]);
-            obj[2] = Number(obj[2]);
-            obj[3] = Number(obj[3]);
-            obj[4] = Number(obj[4]);
-            result.push(obj);
-          }          
-          return result;
-        }, []);
-        console.log(mapped);
-      });
+const fs = require('fs');
+const chokidar = require('chokidar');
+const readLastLines = require('read-last-lines');
+const watcher = chokidar.watch('test.txt');
+
+let ambientPressure = null;
+let pressureDrop = false;
+let peak = false;
+let sampleNumber = 1;
+let lastRead = null;
+let currentRead = null;
+
+watcher.on('change', (event, path, details) => {
+    readLastLines.read('test.txt', 2)
+        .then((lines) => {
+            let line = lines.split(/\r?\n/)[0];
+            lastRead = currentRead;
+            currentRead = createReadObject(line);
+            if (!ambientPressure) {
+                ambientPressure = currentRead.Cell_Pressure;
+            }
+            if (lastRead) {
+                if (!pressureDrop) {
+                    let pressureDelta = ambientPressure - currentRead.Cell_Pressure;
+                    if (pressureDelta > 10) {
+                        console.log("Pressure Drop");
+                        pressureDrop = true;
+                    }
+                }                 
+            }
+            console.log(currentRead)}
+        );
+});
+
+function createReadObject(line) {
+    let obj = line.split( /\s+/);
+    let val = {
+        "System_Time": obj[0],
+        "CO2":  Number(obj[2]),
+        "Cell_Temperature":  Number(obj[2]),
+        "Cell_Pressure":  Number(obj[3]),
+        "Flow_Rate":  Number(obj[4])
+    };
+    return val;
+}
