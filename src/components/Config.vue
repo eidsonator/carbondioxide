689d42122e9eb4a46dfe994e848b1a50d9adc288
Todd Eidson <todd@eidson.info>
2018-05-10 16:29:12 -0400

    Remove read file - add serialport

--- src/components/Config.vue
+++ src/components/Config.vue
@@ -4,22 +4,28 @@
       <div class="column"></div>
       <div class="column box">
         <h3>Settings</h3>
-          <div class="file has-name is-fullwidth is-primary">
-            <label class="file-label">
-              <input class="file-input" id="file" type="file" @change="save()">
-              <span class="file-cta" style="width: 175px;">
-                <span class="file-icon">
-                  <i class="fas fa-upload"></i>
-                </span>
-                <span class="file-label">
-                  Read from file:
-                </span>
-              </span>
-              <span class="file-name">
-                {{ filepath }}
-              </span>
-            </label>
+
+          <div class="field is-horizontal">
+            <div class="field-body">
+              <div class="field is-expanded">
+                <div class="field has-addons">
+                  <p class="control">
+                    <a class="button is-static" style="width: 175px;">
+                      COM:
+                    </a>
+                  </p>
+                  <p class="control is-expanded">
+                    <select class="input" v-model="comName">
+                      <option v-for="port in ports" v-bind:value="port.comName">
+                        {{ port.manufacturer }}
+                      </option>
+                    </select>
+                  </p>
+                </div>
+              </div>
+            </div>
           </div>
+
           <hr>
           <div class="file has-name is-fullwidth is-primary">
           <label class="file-label">
@@ -41,29 +47,6 @@
       <div class="column"></div>
     </div>
 
-    <div class="columns">
-      <div class="column"></div>
-      <div class="column box">
-        <h3>Advanced Settings</h3>
-        <div class="field is-horizontal">
-            <div class="field-body">
-              <div class="field is-expanded">
-                <div class="field has-addons">
-                  <p class="control">
-                    <a class="button is-static" style="width: 175px;">
-                      Lines offset:
-                    </a>
-                  </p>
-                  <p class="control is-expanded">
-                    <input class="input" v-model="linesOffset" @cange="saveOffset()">
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div>
-      </div>
-      <div class="column"></div>
-    </div>
     <hr>
     <button class="button is-primary" @click="clickDone">Done</button>
   </div>
@@ -71,30 +54,33 @@
 
 <script>
 const settings = window.require('electron').remote.require('electron-settings');
+const serialport = window.require('electron').remote.require('serialport');
+
 export default {
   name: "Config",
   data() {
     return {
+      ports: null,
+      comName: settings.get('comName', null),
       exportDirectory: settings.get('exportDirectory'),
-      filepath: settings.get('filepath'),
       linesOffset: settings.get('linesOffset', 4)
     };
   },
+  beforeCreate() {
+    serialport.list((err, ports) => {
+      console.log('ports', ports);
+      this.ports = ports;
+    });
+  },
   methods: {
-    save: function() {
-      let fileName = document.getElementById('file').files[0].path;
-      settings.set('filepath', fileName);
-      this.filepath = settings.get('filepath')
-    },
     setDirectory: function() {
       let exportDirectory = document.getElementById('exportDirectory').files[0].path;
       settings.set('exportDirectory', exportDirectory);
       this.exportDirectory = settings.get('exportDirectory')
     },
-    saveOffset: function() {
-      settings.set('linesOffset', this.linesOffset);
-    },
     clickDone: function() {
+      settings.set('linesOffset', this.linesOffset);
+      settings.set('comName', this.comName);
       this.$router.push({name: 'Collect'});
     }
   },
